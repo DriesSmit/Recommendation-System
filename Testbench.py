807f4340a2323ad204f46762dc17dbd1172dcc35
DriesSmit <dries.epos@gmail.com>
2017-12-08 15:27:02 +0200

    MovieLens database added and new average agorithm added. The total prediction algorithms implemented are now 3.

--- Testbench.py
+++ Testbench.py
@@ -2,7 +2,79 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 import random
+import pandas as pd
 from sklearn.cluster import KMeans
+
+def createData():
+    r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+    ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols,
+                          encoding='latin-1')
+
+    ratings = ratings.drop('unix_timestamp', 1)
+
+    #ratings = ratings[:25]
+
+    sortedRate = ratings.sort_values([('user_id')], ascending=True)
+
+    print(sortedRate)
+
+
+    #print(sortedRate)
+    values = sortedRate.values
+
+    #values = values[:,0:]
+    #print(values)
+    data = {}
+
+    idCount = values[0][0]
+
+    tempHash = {}
+
+    for i in range(len(sortedRate)):
+        #print(values[i][0])
+        if(values[i][0]==idCount):
+            tempHash[values[i][1]] = values[i][2]
+        else:
+            data[values[i-1][0]] = tempHash
+            tempHash = {}
+            tempHash[values[i][1]] = values[i][2]
+            idCount = values[i][0]
+    data[values[i][0]] = tempHash
+    #print(data)
+
+
+
+    '''data = {
+        'Alan Perlis': {
+            'MovieA': 3.0,
+            'MovieB': 5.0,
+            'MovieC': 4.0,
+            'MovieD': 2.0
+        },
+
+        'Dries Smit': {
+            'MovieA': 2.0,
+            'MovieB': 4.0,
+            'MovieC': 4.0,  # ,
+            'MovieD': 2.0
+        },
+
+        'Matthew Mcconaughey': {
+            'MovieA': 1.0,
+            'MovieB': 3.0,
+            'MovieC': 3.0,
+            'MovieD': 1.0
+        },
+
+        'John Connor': {
+            'MovieA': 3.0,
+            'MovieB': 4.0,
+            'MovieC': 3.0,
+            'MovieD': 4.1,
+        },
+    }'''
+
+    return data
 
 # Not good if one person consistently rates more harsly
 def euclidean_similarity(data,person1, person2):
@@ -37,6 +109,10 @@ def pearson_similarity(data,person1, person2):
 
     return (num / den) if den != 0 else 0
 
+def average(data,person1, person2):
+
+    return 1.0
+
 def recommend(data,person, item,bound, similarity=pearson_similarity):
     scores = [(similarity(data,person, other), other) for other in data if other != person]
 
@@ -61,18 +137,23 @@ def recommend(data,person, item,bound, similarity=pearson_similarity):
 
     sim, weight = recomms
 
-    recomms = weight / sim if sim!=0.0 else None#Maybe make this value 2.5 or something like that?
+    recomms = weight / sim if sim!=0.0 else 2.5  # This is a extremely unlikely event of zero correlation found. Maybe
+    # just add 0.0001 to sim to increase speed.
     return recomms
 
 def  evaluate(data,testPerAlg,algs=['euclidean_similarity','pearson_similarity']):
     function_mappings = {
         'euclidean_similarity': euclidean_similarity,
         'pearson_similarity'  : pearson_similarity,
+        'average': average,
     }
     #print("Value: ",data['Dries Smit']['MovieD'])
     results = np.zeros(len(algs))
 
     for test in range(testPerAlg):
+
+        print "Percentage completed: ", round(test*100.0/testPerAlg,2), "%"
+
         randMember = int((len(data) * random.random()))
         memberName = data.keys()[randMember]
         #print "Random member: ", memberName
@@ -85,50 +166,23 @@ def  evaluate(data,testPerAlg,algs=['euclidean_similarity','pearson_similarity']
 
         for i,curAlg in enumerate(algs):
             rec = recommend(data,memberName, itemName, 1, function_mappings[curAlg])
-            #I don't think mean squared error is needed yet
-            results[i] += abs(rec-data['Dries Smit']['MovieD'])
+            # I don't think mean squared error is needed yet. But it is a good practice to implement when actually
+            # using the system.
+            results[i] += abs(rec-data[memberName][itemName])
     results /= testPerAlg
     return results
 
-data = {
-    'Alan Perlis': {
-        'MovieA': 3.0,
-        'MovieB': 5.0,
-        'MovieC': 4.0,
-        'MovieD': 2.0
-    },
+data = createData()
 
-    'Dries Smit': {
-        'MovieA': 2.0,
-        'MovieB': 4.0,
-        'MovieC': 4.0,#,
-        'MovieD': 2.0
-    },
-
-    'Matthew Mcconaughey': {
-        'MovieA': 1.0,
-        'MovieB': 3.0,
-        'MovieC': 3.0,
-        'MovieD': 1.0
-    },
-
-    'John Connor': {
-        'MovieA': 3.0,
-        'MovieB': 4.0,
-        'MovieC': 3.0,
-        'MovieD': 4.1,
-    },
-}
-
-algs = ['euclidean_similarity','pearson_similarity']
-result = evaluate(data,10000,algs=algs)
+algs = ['euclidean_similarity','pearson_similarity','average']
+result = evaluate(data,1000,algs=algs)
 
 print "Results: ", result
 
 x = np.arange(len(algs))
 plt.bar(x, height= result)
 plt.xticks(x+.4, algs)
-plt.title("Error in ratings different algorithms(0 is best)")
+plt.title("Error in ratings for different algorithms(0 is best)")
 plt.xlabel("Algorithm used")
 plt.ylabel("Average error in ratings")
 plt.show()
