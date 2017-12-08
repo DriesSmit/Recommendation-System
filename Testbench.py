ce1021aa4527e161f5634604774b69b4e9e65fe2
DriesSmit <dries.epos@gmail.com>
2017-12-08 12:01:56 +0200

    Testbench algorithm improved.

--- Testbench.py
+++ Testbench.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import random
 from sklearn.cluster import KMeans
 
 # Not good if one person consistently rates more harsly
@@ -20,32 +21,31 @@ def euclidean_similarity(data,person1, person2):
 # compensated for.
 def pearson_similarity(data,person1, person2):
 
-	common_ranked_items = [itm for itm in data[person1] if itm in data[person2]]
+    common_ranked_items = [itm for itm in data[person1] if itm in data[person2]]
 
-	n = len(common_ranked_items)
+    n = len(common_ranked_items)
 
-	s1 = sum([data[person1][item] for item in common_ranked_items])
-	s2 = sum([data[person2][item] for item in common_ranked_items])
+    s1 = sum([data[person1][item] for item in common_ranked_items])
+    s2 = sum([data[person2][item] for item in common_ranked_items])
 
-	ss1 = sum([pow(data[person1][item], 2) for item in common_ranked_items])
-	ss2 = sum([pow(data[person2][item], 2) for item in common_ranked_items])
+    ss1 = sum([pow(data[person1][item], 2) for item in common_ranked_items])
+    ss2 = sum([pow(data[person2][item], 2) for item in common_ranked_items])
 
-	ps = sum([data[person1][item] * data[person2][item] for item in common_ranked_items])
+    ps = sum([data[person1][item] * data[person2][item] for item in common_ranked_items])
+    num = n * ps - (s1 * s2)
+    den = math.sqrt((n * ss1 - math.pow(s1, 2)) * (n * ss2 - math.pow(s2, 2)))
 
-	num = n * ps - (s1 * s2)
-
-	den = math.sqrt((n * ss1 - math.pow(s1, 2)) * (n * ss2 - math.pow(s2, 2)))
-
-	return (num / den) if den != 0 else 0
+    return (num / den) if den != 0 else 0
 
 def recommend(data,person, item,bound, similarity=pearson_similarity):
     scores = [(similarity(data,person, other), other) for other in data if other != person]
 
     scores.sort()
     scores.reverse()
+    #print(scores)
     scores = scores[0:bound]
 
-    print(scores)
+    #print(scores)
 
     recomms = (0.0,0.0)
 
@@ -60,7 +60,8 @@ def recommend(data,person, item,bound, similarity=pearson_similarity):
             recomms = (curSim + sim, curW + weight)
 
     sim, weight = recomms
-    recomms = weight / sim
+
+    recomms = weight / sim if sim!=0.0 else None#Maybe make this value 2.5 or something like that?
     return recomms
 
 def  evaluate(data,testPerAlg,algs=['euclidean_similarity','pearson_similarity']):
@@ -68,17 +69,25 @@ def  evaluate(data,testPerAlg,algs=['euclidean_similarity','pearson_similarity']
         'euclidean_similarity': euclidean_similarity,
         'pearson_similarity'  : pearson_similarity,
     }
-    print("Value: ",data['Dries Smit']['MovieD'])
-    results = {}
+    #print("Value: ",data['Dries Smit']['MovieD'])
+    results = np.zeros(len(algs))
 
-    for i in range(len(algs)):
-        results[str(algs[i])] = 0.0
-        for test in range(testPerAlg):
-            rec = recommend(data,"Dries Smit", 'MovieD', 1, function_mappings[algs[i]])
+    for test in range(testPerAlg):
+        randMember = int((len(data) * random.random()))
+        memberName = data.keys()[randMember]
+        #print "Random member: ", memberName
+        randItem = int((len(data[memberName]) * random.random()))
+        itemName = data[memberName].keys()[randItem]
+        #print "Random item: ", itemName
+
+        #memberName = 'John Connor'
+        #itemName = 'MovieB'
+
+        for i,curAlg in enumerate(algs):
+            rec = recommend(data,memberName, itemName, 1, function_mappings[curAlg])
             #I don't think mean squared error is needed yet
-            results[str(algs[i])] += abs(rec-data['Dries Smit']['MovieD'])
-        results[str(algs[i])] /= testPerAlg
-
+            results[i] += abs(rec-data['Dries Smit']['MovieD'])
+    results /= testPerAlg
     return results
 
 data = {
@@ -107,18 +116,17 @@ data = {
         'MovieA': 3.0,
         'MovieB': 4.0,
         'MovieC': 3.0,
-        'MovieD': 4.0,
+        'MovieD': 4.1,
     },
 }
 
 algs = ['euclidean_similarity','pearson_similarity']
-result = evaluate(data,1,algs=algs)
+result = evaluate(data,10000,algs=algs)
 
-print "Results: ", result.values()
-values = result.values()
+print "Results: ", result
 
 x = np.arange(len(algs))
-plt.bar(x, height= values)
+plt.bar(x, height= result)
 plt.xticks(x+.4, algs)
 plt.title("Error in ratings different algorithms(0 is best)")
 plt.xlabel("Algorithm used")
